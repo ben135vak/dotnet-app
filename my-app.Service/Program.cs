b8755d8a626a521293adcb726f63d3b8ca55c6df
Ben Vaknin <82142247+ben135vak@users.noreply.github.com>
2024-06-03 11:22:59 +0000

    dddd

--- my-app.Service/Program.cs
+++ my-app.Service/Program.cs
@@ -25,7 +25,7 @@ app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1")
 
 app.UseHttpsRedirection();
 
-app.MapGet("/hello", () => "Hello, benn!");
+app.MapGet("/hello", () => "Hello, benn!!!");
 
 app.MapControllers();
 
